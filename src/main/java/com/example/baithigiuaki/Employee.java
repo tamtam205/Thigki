e752ac590b740c881321bbb4d82fdda6edb846a5
Viet Tam <viettam205@gmail.com>
2024-12-10 14:08:47 +0700

    update

---
+++ src/main/java/com/example/baithigiuaki/Employee.java
@@ -0,0 +1,82 @@
+package com.example.baithigiuaki;
+
+public class Employee {
+        private int id;
+        private String hoTen;
+        private int tuoi;
+        private String viTri;
+        private String phongBan;
+        private double luong;
+
+        public Employee( int id, String hoTen, int tuoi, String viTri, String phongBan, double luong ) {
+            this.id = id;
+            this.hoTen = hoTen;
+            this.tuoi = tuoi;
+            this.viTri = viTri;
+            this.phongBan = phongBan;
+            this.luong = luong;
+
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getLuong() {
+            return luong;
+        }
+
+        public void setLuong(double luong) {
+            this.luong = luong;
+        }
+
+        public String getPhongBan() {
+            return phongBan;
+        }
+
+        public void setPhongBan(String phongBan) {
+            this.phongBan = phongBan;
+        }
+
+        public String getViTri() {
+            return viTri;
+        }
+
+        public void setViTri(String viTri) {
+            this.viTri = viTri;
+        }
+
+        public int getTuoi() {
+            return tuoi;
+        }
+
+        public void setTuoi(int tuoi) {
+            this.tuoi = tuoi;
+        }
+
+        public String getHoTen() {
+            return hoTen;
+        }
+
+        public void setHoTen(String hoTen) {
+            this.hoTen = hoTen;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "id=" + id +
+                    ", hoTen='" + hoTen + '\'' +
+                    ", tuoi=" + tuoi +
+                    ", viTri='" + viTri + '\'' +
+                    ", phongBan='" + phongBan + '\'' +
+                    ", luong=" + luong +
+                    '}';
+        }
+    }
+
+
